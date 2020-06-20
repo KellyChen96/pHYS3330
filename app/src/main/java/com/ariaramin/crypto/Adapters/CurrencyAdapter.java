b1a843a25425bc840212bdafa852b428bf959a73
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2020-06-20 14:45:31 +0000

    remove redundant comments

--- app/src/main/java/com/ariaramin/crypto/Adapters/CurrencyAdapter.java
+++ app/src/main/java/com/ariaramin/crypto/Adapters/CurrencyAdapter.java
@@ -3,4 +3,9 @@ package com.ariaramin.crypto.Adapters;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import a
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.Navigation;
+import andr
