7105a321d919d03baa1a0ba9c7b9c8233a7c07f4
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2020-09-06 22:55:35 +0000

    full screen

--- app/src/main/java/com/ariaramin/crypto/Adapters/TopCurrencyAdapter.java
+++ app/src/main/java/com/ariaramin/crypto/Adapters/TopCurrencyAdapter.java
@@ -81,4 +81,6 @@ public class TopCurrencyAdapter extends RecyclerView.Adapter<TopCurrencyAdapter.
         private void loadCoinLogo(DataItem dataItem) {
             Glide.with(topCurrencyLayoutBinding.getRoot().getContext())
                     .load("https://s2.coinmarketcap.com/static/img/coins/64x64/" + dataItem.getId() + ".png")
-                    
+                    .thumbnail(
+                            Glide.with(topCurrencyLayoutBinding.getRoot().getContext())
+                               
