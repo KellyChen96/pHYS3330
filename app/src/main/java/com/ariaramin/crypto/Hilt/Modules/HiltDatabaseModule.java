782a0ef5a7fbf2c0d8ee09fd3e821c00b4c3b59d
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2020-10-11 19:58:04 +0000

    Version updated to 3.6.5

--- app/src/main/java/com/ariaramin/crypto/Hilt/Modules/HiltDatabaseModule.java
+++ app/src/main/java/com/ariaramin/crypto/Hilt/Modules/HiltDatabaseModule.java
@@ -2,4 +2,10 @@ package com.ariaramin.crypto.Hilt.Modules;
 
 import android.content.Context;
 
-import com.ariaramin
+import com.ariaramin.crypto.Room.AppDatabase;
+import com.ariaramin.crypto.Room.DatabaseDao;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagg
