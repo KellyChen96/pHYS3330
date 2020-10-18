530476e72ebf8fe2a015ba8e43458178267e4d61
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2020-10-18 05:48:47 +0000

    Update Git.md

--- app/src/main/java/com/ariaramin/crypto/Hilt/Modules/HiltDatabaseModule.java
+++ app/src/main/java/com/ariaramin/crypto/Hilt/Modules/HiltDatabaseModule.java
@@ -19,3 +19,6 @@ public class HiltDatabaseModule {
 
     @Provides
     @Singleton
+    AppDatabase ProvideAppDatabase(@ApplicationContext Context context) {
+        return AppDatabase.getInstance(context);
+    
