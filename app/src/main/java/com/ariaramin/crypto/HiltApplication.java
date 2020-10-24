f29cef6611fdd7b5bdf7c8c265e84046ff8da0e2
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2020-10-24 23:04:17 +0000

    bump version to 0.2.1

--- app/src/main/java/com/ariaramin/crypto/HiltApplication.java
+++ app/src/main/java/com/ariaramin/crypto/HiltApplication.java
@@ -2,4 +2,8 @@ package com.ariaramin.crypto;
 
 import android.app.Application;
 
-import dagger.hilt.android.Hilt
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class HiltApplication extends Application {
+}
---
+++ app/src/main/java/com/ariaramin/crypto/MainActivity.java
@@ -0,0 +1,56 @@
+
+package com.ariaramin.crypto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkRequest;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.ariaramin.crypto.Models.AllMarket;
+import com.ariaramin.crypto.databinding.ActivityMainBinding;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import me.ibrahimsn.lib.SmoothBottomBar;
+
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
+    MainViewModel mainViewModel;
+    CompositeDisposable compositeDisposable;
+    @Inject
+    ConnectivityManager connectivityManager;
+    @Inject
+    NetworkRequest networkRequest;
+    public SmoothBottomBar smoothBottomBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        smoothBottomBar = activityMainBinding.bottomBar;
