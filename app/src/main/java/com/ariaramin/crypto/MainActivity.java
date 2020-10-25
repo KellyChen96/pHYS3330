4ae87c4efa1f2afe5eea6da52642ed063041b746
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2020-10-25 07:37:40 +0000

    Incidencias

--- app/src/main/java/com/ariaramin/crypto/MainActivity.java
+++ app/src/main/java/com/ariaramin/crypto/MainActivity.java
@@ -54,3 +54,81 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         smoothBottomBar = activityMainBinding.bottomBar;
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        compositeDisposable = new CompositeDisposable();
+
+        setupNavigationComponent();
+        checkNetworkConnection();
+    }
+
+    private void checkNetworkConnection() {
+        ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@androidx.annotation.NonNull Network network) {
+                super.onAvailable(network);
+                callApiRequest();
+            }
+
+            @Override
+            public void onLost(@androidx.annotation.NonNull Network network) {
+                super.onLost(network);
+                Toast.makeText(getApplicationContext(), "Check your network connection.", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            connectivityManager.registerDefaultNetworkCallback(networkCallback);
+        } else {
+            connectivityManager.registerNetworkCallback(networkRequest, networkCallback);
+        }
+    }
+
+    private void callApiRequest() {
+        Observable.interval(5, TimeUnit.SECONDS)
+                .flatMap(n -> mainViewModel.getAllMarket())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<AllMarket>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        compositeDisposable.add(d);
+                    }
+
+                    @Override
+                    public void onNext(@NonNull AllMarket allMarket) {
+                        // Get data from API and insert into database
+                        mainViewModel.insertAllMarket(allMarket);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    private void setupNavigationComponent() {
+        // Get nav host and nav controller
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        setupSmoothBottomMenu(navController);
+    }
+
+    private void setupSmoothBottomMenu(NavController navController) {
+        PopupMenu popupMenu = new PopupMenu(this, null);
+        popupMenu.inflate(R.menu.bottom_nav_menu);
+        Menu menu = popupMenu.getMenu();
+        activityMainBinding.bottomBar.setupWithNavController(menu, navController);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
+    }
+}
---
+++ app/src/main/java/com/ariaramin/crypto/MainRepository.java
@@ -0,0 +1,5 @@
+package com.ariaramin.crypto;
+
+import android.util.Log;
+
+import co
