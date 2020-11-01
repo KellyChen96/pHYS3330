fc0076bb0e716343fa3a1df5190af5918237893d
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2020-11-01 10:09:27 +0000

    wcnss: logging enhancement with IRIS register dump at Wdog bite

--- app/src/main/java/com/ariaramin/crypto/MainRepository.java
+++ app/src/main/java/com/ariaramin/crypto/MainRepository.java
@@ -4,4 +4,8 @@ import android.util.Log;
 
 import com.ariaramin.crypto.Models.AllMarket;
 import com.ariaramin.crypto.Retrofit.RequestApi;
-import com.ariaramin.crypto.Room.Database
+import com.ariaramin.crypto.Room.DatabaseDao;
+import com.ariaramin.crypto.Room.Entities.AllMarketEntity;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+impo
