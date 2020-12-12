c8340439cf61f6953863b5062d38a31d82ad5584
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2020-12-12 11:24:02 +0000

    Add initial version.

--- app/src/main/java/com/ariaramin/crypto/MainViewModel.java
+++ app/src/main/java/com/ariaramin/crypto/MainViewModel.java
@@ -7,4 +7,10 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import com.ariaramin.crypto.Models.AllMarket;
-import com.ariaramin.crypto.Room.Ent
+import com.ariaramin.crypto.Room.Entities.AllMarketEntity;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.Hi
