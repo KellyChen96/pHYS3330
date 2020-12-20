b9e4663fa0ff119f17548fa21776c65f27321928
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2020-12-20 18:51:31 +0000

    Applied Google style conventions

--- app/src/main/java/com/ariaramin/crypto/Models/AllMarket.java
+++ app/src/main/java/com/ariaramin/crypto/Models/AllMarket.java
@@ -1,3 +1,12 @@
 package com.ariaramin.crypto.Models;
 
 import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class AllMarket {
+
+    @SerializedName("data")
+    private Data data;
+
+    @SerializedName("statu
