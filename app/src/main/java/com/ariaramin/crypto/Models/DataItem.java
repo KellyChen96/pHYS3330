db5eb947409f7c44a699f6c8a9449d875b7a3a7e
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2021-01-09 09:31:30 +0000

    mm: cma: skip watermarks check for already isolated blocks in split_free_page()

--- app/src/main/java/com/ariaramin/crypto/Models/DataItem.java
+++ app/src/main/java/com/ariaramin/crypto/Models/DataItem.java
@@ -1,4 +1,11 @@
 package com.ariaramin.crypto.Models;
 
 import android.os.Parcel;
-import android.os.Parcelable
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataItem implements 
