f906421647563a0cbe54b2dbb1aea0deb574d03b
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2021-04-25 07:51:06 +0000

    Test change

--- app/src/main/java/com/ariaramin/crypto/Retrofit/RequestApi.java
+++ app/src/main/java/com/ariaramin/crypto/Retrofit/RequestApi.java
@@ -2,4 +2,9 @@ package com.ariaramin.crypto.Retrofit;
 
 import com.ariaramin.crypto.Models.AllMarket;
 
-import io.reactivex.rxjava3.core.Obser
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.GET;
+
+public interface RequestApi {
+
+    @GET("cryptocurrency/listing?start=1&limit=500&sortBy=market_
