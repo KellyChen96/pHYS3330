115aa5890a916e700c248501ead42a99dcae8acd
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2021-04-25 19:31:01 +0000

    Compile assets

--- app/src/main/java/com/ariaramin/crypto/Retrofit/RequestApi.java
+++ app/src/main/java/com/ariaramin/crypto/Retrofit/RequestApi.java
@@ -7,4 +7,6 @@ import retrofit2.http.GET;
 
 public interface RequestApi {
 
-    @GET("cryptocurrency/listing?start=1&limit=500&sortBy=market_cap&sortType=desc&convert=USD&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added
+    @GET("cryptocurrency/listing?start=1&limit=500&sortBy=market_cap&sortType=desc&convert=USD&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,tags,platform,max_supply,circulating_supply,total_supply,volume_7d,volume_30d")
+    Observable<AllMarket> getMarketList();
+}
