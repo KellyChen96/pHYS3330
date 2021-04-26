84dc6637ca2a56981dd3b0a9940294316fbe501b
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2021-04-26 07:11:12 +0000

    Changes after review.

--- app/src/main/java/com/ariaramin/crypto/Room/AppDatabase.java
+++ app/src/main/java/com/ariaramin/crypto/Room/AppDatabase.java
@@ -5,4 +5,7 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import 
+import androidx.room.TypeConverters;
+
+import com.ariaramin.crypto.Room.Converters.AllMarketConverter;
+i
