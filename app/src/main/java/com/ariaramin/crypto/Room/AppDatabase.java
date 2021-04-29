c120f24bcb8204192350ebc87397a738afd32f8a
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2021-04-29 19:44:39 +0000

    Update scalatest to 3.2.3

--- app/src/main/java/com/ariaramin/crypto/Room/AppDatabase.java
+++ app/src/main/java/com/ariaramin/crypto/Room/AppDatabase.java
@@ -20,4 +20,7 @@ public abstract class AppDatabase extends RoomDatabase {
     public static AppDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(
-     
+                    context.getApplicationContext(),
+                    AppDatabase.class,
+                    "crypto_db")
+           
