c82af8dab157bf2cfb0a7805290480ca5e95263f
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2021-09-29 20:11:18 +0000

    cosmetic change.

--- app/src/main/java/com/ariaramin/crypto/ui/Fragments/HomeFragment.java
+++ app/src/main/java/com/ariaramin/crypto/ui/Fragments/HomeFragment.java
@@ -3,4 +3,7 @@ package com.ariaramin.crypto.ui.Fragments;
 import android.content.Context;
 import android.os.Bundle;
 
-import androidx.
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Vie
