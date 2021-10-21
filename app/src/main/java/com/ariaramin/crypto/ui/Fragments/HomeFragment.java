a7df3b258df5703b72db6182c60d1c3d6159a412
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2021-10-21 14:28:20 +0000

    adding title

--- app/src/main/java/com/ariaramin/crypto/ui/Fragments/HomeFragment.java
+++ app/src/main/java/com/ariaramin/crypto/ui/Fragments/HomeFragment.java
@@ -29,4 +29,8 @@ import java.util.List;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.disposables.CompositeDispo
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+@Androi
