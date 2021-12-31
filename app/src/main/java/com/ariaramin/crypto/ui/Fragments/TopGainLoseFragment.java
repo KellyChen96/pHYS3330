699222f54537c6bddbfe06684d7ec74d762c57ba
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2021-12-31 07:54:27 +0000

    Syntax highlighting

--- app/src/main/java/com/ariaramin/crypto/ui/Fragments/TopGainLoseFragment.java
+++ app/src/main/java/com/ariaramin/crypto/ui/Fragments/TopGainLoseFragment.java
@@ -2,4 +2,8 @@ package com.ariaramin.crypto.ui.Fragments;
 
 import android.os.Bundle;
 
-import androidx.databinding.DataBi
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInfla
