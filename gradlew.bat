df5404857dfc064a60b6fe44fda4e72f2a4df94a
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2023-03-24 11:59:15 +0000

    fixed deprecation warnings

--- gradlew.bat
+++ gradlew.bat
@@ -14,4 +14,7 @@
 @rem limitations under the License.
 @rem
 
-@if "%DEBUG%" == "" @
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Wind
