42e9952d333338e5451c23ac776f50825fab4abf
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2023-03-25 12:29:42 +0000

    index change

--- gradlew.bat
+++ gradlew.bat
@@ -26,4 +26,8 @@ if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
