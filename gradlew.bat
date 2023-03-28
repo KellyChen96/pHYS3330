17fcf3ff3c56b76804cbed5911c17023671adb48
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2023-03-28 21:45:22 +0000

    adding license header

--- gradlew.bat
+++ gradlew.bat
@@ -38,4 +38,9 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-set JAVA_EXE=ja
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' comm
