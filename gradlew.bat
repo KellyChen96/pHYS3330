241f261cc6e48401d7fbbfc6762b726e5c096860
KellyChen96 <110959628+KellyChen96@users.noreply.github.com>
2023-03-31 12:55:18 +0000

    fix typo in test name

--- gradlew.bat
+++ gradlew.bat
@@ -65,4 +65,10 @@ echo location of your Java installation.
 goto fail
 
 :execute
-@rem Setup the command
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OP
